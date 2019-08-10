08f84a1f3e757fd7e9110e1ca054fa5d7bad69ab
Carlos Mostek <carlos.mostek@auth0.com>
2019-08-10 18:39:22 -0500

    add payment method to invoice

--- client/containers/Orders/Order.jsx
+++ client/containers/Orders/Order.jsx
@@ -213,6 +213,7 @@ TAX ID: ___________________________________________
                 <div>TARGET SHIP DATE: {order.shipAsap ? 'ASAP' : ''} {moment.unix(order.targetShipDate).format('MM/DD/YYYY')}</div> : ''}
               {order.shippedDate ? <div>SHIPPED DATE: {moment.unix(order.shippedDate).format('MM/DD/YYYY')}</div> : ''}
               {order.show && order.show.name !== 'House Account' ? <div>SHOW: {order.show.name}</div> : ''}
+              PAYMENT METHOD: {order.store.paymentTerms && order.store.paymentTerms.method ? order.store.paymentTerms.method : 'any' }
               <br/>
               <OrderStoreInfo store={order.store}/>
             </div>
